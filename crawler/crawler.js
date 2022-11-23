08bc5e3b7031e5372b548ed4eb1c867c1a4cb8f7
minhdl <longminhdo2000@gmail.com>
2022-11-23 10:40:29 +0700

    feat: update append file instead of writefile

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -13,6 +13,13 @@ const writeFile = (fileName, data) => {
   });
 };
 
+const writeFile2 = (fileName, data) => {
+  // return console.log(data);
+  fsLibrary.appendFile(fileName, data, (error) => {
+    if (error) throw err;
+  });
+};
+
 const execution = async () => {
   for (let chunk = 0; chunk < 200; chunk++) {
     const result = [];
@@ -40,4 +47,33 @@ const execution = async () => {
   }
 };
 
-execution();
+const execution2 = async () => {
+  for (let chunk = 0; chunk < 200; chunk++) {
+    const result = [];
+    for (let i = chunk * 5000; i < (chunk + 1) * 5000; i += 500) {
+      const promises = [];
+      for (let movieId = i; movieId <= i + 500; movieId++) {
+        promises.push(
+          fetch(
+            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
+          )
+        );
+      }
+
+      const res = await Promise.all(promises);
+      for (let j = 0; j < res?.length; j++) {
+        if (res[j]?.status === 200) {
+          const data = await res[j].json();
+          result.push(data);
+        }
+      }
+      console.log(`${i + 500}/1000000`);
+    }
+
+    writeFile2(`movies.json`, `${JSON.stringify(result).slice(1, -1)},`);
+  }
+};
+
+// execution();
+
+execution2();
