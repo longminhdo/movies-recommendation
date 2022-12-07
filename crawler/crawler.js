91c1173de6a423c0e5cedd9402d4a50b71236d61
minhdl <longminhdo2000@gmail.com>
2022-12-07 07:47:57 +0700

    file exploration

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,14 +1,14 @@
-import * as dotenv from "dotenv";
-import fetch from "node-fetch";
-import * as fsLibrary from "fs";
+import * as dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import * as fsLibrary from 'fs';
 
 globalThis.fetch = fetch;
 dotenv.config();
 
 const apiKey = process.env.API_KEY;
 
-const writeFile = (fileName, data) => {
-  fsLibrary.writeFile(fileName, data, (error) => {
+const writeFile = async (fileName, data) => {
+  await fsLibrary.writeFile(fileName, data, (error) => {
     if (error) throw err;
   });
 };
@@ -23,24 +23,27 @@ const writeFile2 = (fileName, data) => {
 const execution = async () => {
   for (let chunk = 0; chunk < 200; chunk++) {
     const result = [];
-    for (let i = chunk * 5000; i < (chunk + 1) * 5000; i += 500) {
+    for (let i = chunk * 5000; i < (chunk + 1) * 5000; i += 250) {
       const promises = [];
-      for (let movieId = i; movieId <= i + 500; movieId++) {
+      for (let movieId = i; movieId <= i + 250; movieId++) {
         promises.push(
           fetch(
             `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
           )
         );
       }
-
-      const res = await Promise.all(promises);
-      for (let j = 0; j < res?.length; j++) {
-        if (res[j]?.status === 200) {
-          const data = await res[j].json();
-          result.push(data);
+      try {
+        const res = await Promise.all(promises);
+        for (let j = 0; j < res?.length; j++) {
+          if (res[j]?.status === 200) {
+            const data = await res[j].json();
+            result.push(data);
+          }
         }
+      } catch (error) {
+        console.log(error);
       }
-      console.log(`${i + 500}/1000000`);
+      console.log(`${i + 250}/1000000`);
     }
 
     writeFile(`movies_${chunk}.json`, JSON.stringify(result));
@@ -74,6 +77,6 @@ const execution2 = async () => {
   }
 };
 
-// execution();
+execution();
 
-execution2();
+// execution2();
