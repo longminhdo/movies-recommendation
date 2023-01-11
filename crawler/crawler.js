77777ef936b21d34d0d4bdaa8fc8e5bfa54f10b1
minhdl <longminhdo2000@gmail.com>
2023-01-11 07:57:04 +0700

    chore: remove unused code

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -28,8 +28,8 @@ const execution = async () => {
       for (let movieId = i; movieId <= i + 250; movieId++) {
         promises.push(
           fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
-          )
+            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`,
+          ),
         );
       }
       try {
@@ -50,33 +50,4 @@ const execution = async () => {
   }
 };
 
-const execution2 = async () => {
-  for (let chunk = 0; chunk < 200; chunk++) {
-    const result = [];
-    for (let i = chunk * 5000; i < (chunk + 1) * 5000; i += 500) {
-      const promises = [];
-      for (let movieId = i; movieId <= i + 500; movieId++) {
-        promises.push(
-          fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
-          )
-        );
-      }
-
-      const res = await Promise.all(promises);
-      for (let j = 0; j < res?.length; j++) {
-        if (res[j]?.status === 200) {
-          const data = await res[j].json();
-          result.push(data);
-        }
-      }
-      console.log(`${i + 500}/1000000`);
-    }
-
-    writeFile2(`movies.json`, `${JSON.stringify(result).slice(1, -1)},`);
-  }
-};
-
 execution();
-
-// execution2();
