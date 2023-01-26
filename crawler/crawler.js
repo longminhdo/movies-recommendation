54c812c836cf0fa87ad006b7207cdc706efb082f
minhdl <longminhdo2000@gmail.com>
2023-01-26 15:38:58 +0700

    feat: get ground truth

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -5,6 +5,8 @@ import * as fsLibrary from 'fs';
 globalThis.fetch = fetch;
 dotenv.config();
 
+const TestIdList = [];
+
 const apiKey = process.env.API_KEY;
 
 const writeFile = async (fileName, data) => {
@@ -30,7 +32,9 @@ const execution = async () => {
         for (let j = 0; j < res?.length; j++) {
           if (res[j]?.status === 200) {
             const data = await res[j].json();
-            result.push(data);
+            if (data?.genres?.length > 0 && data?.overview !== '') {
+              result.push(data);
+            }
           }
         }
       } catch (error) {
@@ -41,6 +45,38 @@ const execution = async () => {
 
     writeFile(`movies_${chunk}.json`, JSON.stringify(result));
   }
+};
+
+const getGroundTruth = async () => {
+  const result = [];
+
+  for (let i = 0; i < TestIdList?.length; i += 200) {
+    const promises = [];
+    //https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=ae91cd69ae08a3b30879b9cb8a77e539
+    for (let j = i; j <= i + 200; j++) {
+      const movieId = TestIdList[j];
+      promises.push(
+        fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=ae91cd69ae08a3b30879b9cb8a77e539`,
+        ),
+      );
+    }
+
+    try {
+      const res = await Promise.all(promises);
+      for (let j = 0; j < res?.length; j++) {
+        if (res[j]?.status === 200) {
+          const data = await res[j].json();
+          //handle result here
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(`${i + 200}/5000`);
+  }
+
+  writeFile(`movies_${chunk}.json`, JSON.stringify(result));
 };
 
 execution();
