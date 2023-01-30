48e08fe94980f2c1f0224f1c4c27b07c7c6b4473
Tung Lam <daongtunglam1611@gmail.com>
2023-01-30 12:51:31 +0700

    Updated data cleaning

--- data_cleaning/data_preprocessing.py
+++ data_cleaning/data_preprocessing.py
@@ -1,27 +1,56 @@
+import json
+import numpy as np
 import pandas as pd
 import re
-from ast import literal_eval
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
-df = pd.read_csv('../data/movies.csv')
+df = pd.read_csv('data/movies.csv')
+
+print('Data Cleaning...', end='\t')
+# Remove irrelevant / redundant fields
+fields = ['adult', 'belongs_to_collection', 'budget', 'genres', 'id', 'original_language', 'overview', 'popularity', 
+          'production_companies', 'release_date', 'revenue', 'runtime', 'status', 'title', 'vote_average', 'vote_count']
+df = df[fields]
 
 # Keep only the name of `genres`
-df['genres'] = df['genres'].apply(literal_eval)
+df['genres'] = df['genres'].apply(json.loads)
 df['genres'] = df['genres'].apply(lambda x: [i['name'] for i in x])
 
 # Keep only the name of `production_companies`
-df['production_companies'] = df['production_companies'].apply(literal_eval)
+df['production_companies'] = df['production_companies'].apply(json.loads)
 df['production_companies'] = df['production_companies'].apply(lambda x: [i['name'] for i in x])
 
-# # Handle missing value of `budget` - replace with median value
-# median_budget = df[df['budget'] != 0]['budget'].median()
-# df.loc[df['budget'] == 0, 'budget'] = median_budget
+# Remove missing value of `genres` & `overview`
+df['genres'].replace('[]', np.nan, inplace=True)
+df['overview'].replace('^\s*$', np.nan, regex=True, inplace=True)
+df['overview'].replace('No overview found.', np.nan, inplace=True)
+df.dropna(subset=['genres', 'overview'], inplace=True)
 
-# # Handle missing value of `revenue` - replace with median value
-# median_revenue = df[df['revenue'] != 0]['revenue'].median()
-# df.loc[df['revenue'] == 0, 'revenue'] = median_revenue
+# Handle missing value of `budget` & `revenue`- replace with average of median values group by genres
+def fill_missing_budget_revenue(df):
+    df2 = df.query('budget != 0')
+    df3 = df.query('revenue != 0')
 
+    genre_list = df['genres'].explode().unique()
+    budget_dict = {}
+    revenue_dict = {}
+
+    for genre in genre_list:
+        budget_dict[genre] = df2[df2['genres'].apply(lambda x: genre in x)]['budget'].median()
+        revenue_dict[genre] = df3[df3['genres'].apply(lambda x: genre in x)]['revenue'].median()
+        
+    def avg_median(row, median_dict):
+        genre_medians = [median_dict[g] for g in row['genres']]
+        return sum(genre_medians) / len(genre_medians)
+    
+    df['budget'] = df.apply(lambda row: avg_median(row, budget_dict) if row['budget'] == 0 else row['budget'], axis=1)
+    df['revenue'] = df.apply(lambda row: avg_median(row, revenue_dict) if row['revenue'] == 0 else row['revenue'], axis=1)
+
+fill_missing_budget_revenue(df)
+print('Done')
+
+print('Feature Engineering...', end='\t')
 # Create `keywords` field
 stop_words = set(stopwords.words('english'))
 stemmer = SnowballStemmer("english")
@@ -47,5 +76,6 @@ def combine_features_to_string(item_list):
 df['metadata'] = df['genres'].apply(combine_features_to_string) + ' ' + \
                  df['production_companies'].apply(combine_features_to_string) + ' ' + \
                  df['original_language']
+print('Done')
 
 df.to_csv('data/movies_clean.csv', index=False)
